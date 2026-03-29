46be56141ba6dd824020647ab1e67edf620b3245
Maksim Belov <ultramaksim@gmail.com>
2026-03-30 00:05:57 +0300

    Feat[jdk]: update to 21.0.11+8, 25.0.3+7

--- 5_clonejdk.sh
+++ 5_clonejdk.sh
@@ -2,9 +2,9 @@
 set -e
 
 if [[ $TARGET_VERSION -eq 21 ]]; then
-    git clone --branch "jdk-21.0.10+6" --depth 1 https://github.com/openjdk/jdk21u openjdk-21
+    git clone --branch "jdk-21.0.11+8" --depth 1 https://github.com/openjdk/jdk21u openjdk-21
 elif [[ $TARGET_VERSION -eq 25 ]]; then
-    git clone --depth 1 https://github.com/openjdk/jdk25u openjdk-25
+    git clone --branch "jdk-25.0.3+7" --depth 1 https://github.com/openjdk/jdk25u openjdk-25
 else
     git clone --depth 1 https://github.com/openjdk/jdk17u openjdk-17
 fi
